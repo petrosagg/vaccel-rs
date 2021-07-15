ba867d2c378b6568d030bfcd38a8e816a427e46a
Petros Angelatos <petrosagg@gmail.com>
2021-07-15 16:43:21 +0200

    implement client and add a full test. also switch to concurrent hashmap
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

---
+++ src/client.rs
@@ -0,0 +1,66 @@
+use std::marker::PhantomData;
+
+use tarpc::client;
+use tarpc::context;
+use tarpc::server::{BaseChannel, Channel};
+use tarpc::transport::channel;
+
+use crate::resource::{Resource, ResourceRef};
+use crate::session::{VaccelClient, VaccelHandler, Vaccel};
+
+pub enum ClientConfig {
+    /// All RPC calls will be performed over an in-memory mpsc channel
+    Local,
+    /// All RPC calls will be performed over a vsock socket
+    Remote,
+}
+
+pub struct Client {
+    inner: VaccelClient,
+}
+
+impl Client {
+    pub fn new(config: ClientConfig) -> Self {
+        match config {
+            ClientConfig::Local => {
+                let (client_transport, server_transport) = channel::unbounded();
+                let server = BaseChannel::with_defaults(server_transport);
+                tokio::spawn(server.execute(VaccelHandler::default().serve()));
+
+                Self {
+                    inner: VaccelClient::new(client::Config::default(), client_transport).spawn(),
+                }
+            }
+            ClientConfig::Remote => {
+                todo!("create vsock transport")
+            }
+        }
+    }
+
+    pub async fn register<T: Into<Resource>>(&self, data: T) -> ResourceRef<T> {
+        let id = self.inner.register_resource(context::current(), data.into()).await.unwrap();
+        ResourceRef { id, marker: PhantomData }
+    }
+
+    pub async fn length(&self, model: ResourceRef<String>) -> usize {
+        self.inner.length(context::current(), model).await.unwrap()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[tokio::test]
+    async fn basic_client() {
+        let client = Client::new(ClientConfig::Local);
+
+        let resource = client.register("cat".to_string()).await;
+        let resp = client.length(resource).await;
+        assert_eq!(resp, 3);
+
+        let resource = client.register("foobar".to_string()).await;
+        let resp = client.length(resource).await;
+        assert_eq!(resp, 6);
+    }
+}
--- src/session.rs
+++ src/session.rs
@@ -1,81 +1,51 @@
 use std::convert::TryInto;
-use std::collections::HashMap;
-use std::marker::PhantomData;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicU64, Ordering};
 
 use futures::future::{self, Ready};
-use tarpc::context::{self, Context};
+use dashmap::DashMap;
+use tarpc::context::Context;
+
 use crate::resource::{Resource, ResourceRef};
 
 #[tarpc::service]
-trait Vaccel {
+pub trait Vaccel {
+    /// Core method that registers a resource for future use
     async fn register_resource(resource: Resource) -> u64;
 
-    // Image ops
-    async fn segmentation(image: ResourceRef<Vec<u8>>) -> u64;
-
-    // Tensorflow ops
-    async fn inference(model: ResourceRef<Vec<u8>>) -> String;
+    /// Example operation that returns the legnth of the passed resource
+    async fn length(data: ResourceRef<String>) -> usize;
 }
 
 #[derive(Default, Clone)]
-struct VaccelHandler(Arc<Mutex<VaccelState>>);
+pub struct VaccelHandler(Arc<VaccelState>);
 
 #[derive(Default)]
-struct VaccelState {
-    resource_id: u64,
-    resources: HashMap<u64, Arc<Resource>>,
+pub struct VaccelState {
+    resource_id: AtomicU64,
+    resources: DashMap<u64, Arc<Resource>>,
 }
 
 impl VaccelHandler {
-    pub async fn register<T: Into<Resource>>(&self, data: T) -> ResourceRef<T> {
-        let id = self.clone().register_resource(context::current(), data.into()).await;
-        ResourceRef { id, marker: PhantomData }
-    }
-
     pub fn get_resource<T>(&self, resource: &ResourceRef<T>) -> Option<Arc<Resource>> {
-        let this = self.0.lock().unwrap();
-        this.resources.get(&resource.id).cloned()
+        self.0.resources.get(&resource.id).map(|r| Arc::clone(r.value()))
     }
 }
 
 impl Vaccel for VaccelHandler {
     type RegisterResourceFut = Ready<u64>;
     fn register_resource(self, _: Context, resource: Resource) -> Self::RegisterResourceFut {
-        let mut this = self.0.lock().unwrap();
-        this.resource_id += 1;
-        let id = this.resource_id;
-        this.resources.insert(id, Arc::new(resource));
+        let id = self.0.resource_id.fetch_add(1, Ordering::SeqCst);
+        self.0.resources.insert(id, Arc::new(resource));
         future::ready(id)
     }
 
-    type SegmentationFut = Ready<u64>;
-    fn segmentation(self, _: Context, _image: ResourceRef<Vec<u8>>) -> Self::SegmentationFut {
-        future::ready(42)
-    }
+    type LengthFut = Ready<usize>;
+    fn length(self, _: Context, data: ResourceRef<String>) -> Self::LengthFut {
+        // TODO: this is ugly, lift this logic in get_resource and make that return an Option<&T>
+        let resource = self.get_resource(&data).unwrap();
+        let data: &String = (&*resource).try_into().unwrap();
 
-    type InferenceFut = Ready<String>;
-    fn inference(self, _: Context, _model: ResourceRef<Vec<u8>>) -> Self::InferenceFut {
-        future::ready("cat".to_string())
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-    use tarpc::server::{BaseChannel, Channel};
-    use tarpc::transport::channel;
-    use tarpc::client;
-
-    #[tokio::test]
-    async fn it_works() {
-        let (client_transport, server_transport) = channel::unbounded();
-        let server = BaseChannel::with_defaults(server_transport);
-        tokio::spawn(server.execute(VaccelHandler::default().serve()));
-        let mut client = VaccelClient::new(client::Config::default(), client_transport).spawn();
-
-        let fake_resource = ResourceRef { id: 0, marker: std::marker::PhantomData };
-        let resp = client.inference(context::current(), fake_resource).await.unwrap();
-        assert_eq!(&resp, "cat");
+        future::ready(data.len())
     }
 }
